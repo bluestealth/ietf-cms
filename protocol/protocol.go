fbb9fd4571f7c3b9df465585725552d07913965a
Dino A. Dai Zovi <ddz@theta44.org>
2021-06-12 16:32:42 +0000

    Sort SignedAttrs in AddSignerInfo().
    X690 Section 11.6 specifies a required order for SET OFs in DER.
    Go 1.15 updated encoding/asn1 to perform this sorting, which caused
    a discrepancy between the order of the attributes in SignerInfo
    and the order in the marshaled form generated by MarshaledForSigning().
    
    The attributes are now sorted before calling MarshaledforSigning()
    to ensure consistency. This fixes interoperability with other systems
    (e.g. OpenSSL CMS) for signatures generated by ietf-cms when compiled
    with Go >= 1.15.

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"sort"
 	"time"
 
 	"github.com/github/ietf-cms/oid"
@@ -683,7 +684,11 @@ func (sd *SignedData) AddSignerInfo(chain []*x509.Certificate, signer crypto.Sig
 		return err
 	}
 
-	si.SignedAttrs = append(si.SignedAttrs, stAttr, mdAttr, ctAttr)
+	// sort attributes to match required order in marshaled form
+	si.SignedAttrs, err = sortAttributes(stAttr, mdAttr, ctAttr)
+	if err != nil {
+		return err
+	}
 
 	// Signature is over the marshaled signed attributes
 	sm, err := si.SignedAttrs.MarshaledForSigning()
@@ -703,6 +708,19 @@ func (sd *SignedData) AddSignerInfo(chain []*x509.Certificate, signer crypto.Sig
 	sd.SignerInfos = append(sd.SignerInfos, si)
 
 	return nil
+}
+
+func sortAttributes(attrs ...Attribute) ([]Attribute, error) {
+	// Sort attrs by their encoded values (including tag and
+	// lengths) as specified in X690 Section 11.6 and implemented
+	// in go >= 1.15's asn1.Marshal().
+	sort.Slice(attrs, func(i, j int) bool {
+		return bytes.Compare(
+			attrs[i].RawValue.FullBytes,
+			attrs[j].RawValue.FullBytes) < 0
+	})
+
+	return attrs, nil
 }
 
 // algorithmsForPublicKey takes an opinionated stance on what algorithms to use
